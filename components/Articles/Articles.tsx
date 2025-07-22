a1747841ba5ff4d4508aa83ea4848d53a047a045
streesmos <61740996+streesmos@users.noreply.github.com>
2025-07-23 00:42:16 +0300

    Update Articles.tsx

--- components/Articles/Articles.tsx
+++ components/Articles/Articles.tsx
@@ -1,86 +1,126 @@
 "use client";
 
 import { useArticleContext } from "@/hooks/useArticleContext";
+import Sidebar from "../Sidebar";
+import { Separator } from "@radix-ui/react-separator";
 import Link from "next/link";
-import { Button } from "../ui/button";
-import { useState } from "react";
+import Loading from "./loading";
+import Image from "next/image";
 
-export default function Articles() {
+export default function LatestArticles() {
   const { data } = useArticleContext();
-  const [selectedLabel, setSelectedLabel] = useState("Все");
 
-  const labels: string[] = [
-    "Все",
-    ...new Set(
-      data.flatMap((article) => article.articles.map((item) => item.label))
-    ),
-  ];
+  if (data.length > 0 && data[0].articles.length > 0) {
+    const allArticles = data[0].articles.sort((a, b) => {
+      const dateA = new Date(a.date).getTime();
+      const dateB = new Date(b.date).getTime();
+      return dateB - dateA;
+    });
 
-  const filteredArticles = data.flatMap((article) =>
-    article.articles
-      .filter((item) =>
-        selectedLabel === "Все" ? true : selectedLabel === item.label
-      )
-      .map((item) => ({
-        ...item,
-        author: article.author,
-      }))
-  );
+    const latestArticle = allArticles[0];
 
-  return (
-    <div className="max-w-[95rem] w-full mx-auto">
-      <div className="flex flex-wrap justify-between items-center gap-2 md:gap-0 my-6">
-        <p className="font-semibold uppercase">Категории</p>
-        {data && (
-          <div className="flex flex-wrap gap-2">
-            {labels.map((label, index) => (
-              <Button
-                className={`px-3 py-2 bg-white text-black hover:bg-black hover:text-white border border-black rounded-full ${
-                  label === selectedLabel
-                    ? "bg-black text-white"
-                    : "border-black"
-                }`}
-                key={index}
-                onClick={() => setSelectedLabel(label)}
-              >
-                {label}
-              </Button>
-            ))}
-          </div>
-        )}
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 border border-black border-collapse mb-48">
-        {filteredArticles.map((articleData, index) => (
-          <article className="border border-black p-8" key={index}>
-            <div className="flex items-center justify-between">
-              <time dateTime={articleData.date}>{articleData.date}</time>
-              <span className="px-3 py-2 border border-black rounded-full">
-                <p className="uppercase">{articleData.label}</p>
-              </span>
-            </div>
-            <Link href={`magazine/${articleData.slug}`}>
-              <img
-                className="w-full my-8 hover:scale-105 transition"
-                src={articleData.img}
-                alt={articleData.imgAlt}
+    const remainingArticles = allArticles.slice(1);
+
+    return (
+      <>
+        <div className="flex flex-col-reverse sm:flex-col gap-6 md:gap-12 py-6 md:py-10 max-w-[95rem] w-full mx-auto">
+          <article className="flex flex-col-reverse sm:flex-col gap-6 md:gap-12">
+            <article className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12">
+              <h2 className="text-subtitle">
+                <Link href={`/magazine/${latestArticle.slug}`}>
+                  {latestArticle.title}
+                </Link>
+              </h2>
+              <article className="flex flex-col justify-between gap-2">
+                <p>{latestArticle.description}</p>
+                <div className="flex flex-col sm:flex-row justify-between sm:items-center gap-2">
+                  <div className="flex flex-col sm:flex-row gap-2 sm:gap-6">
+                    <span className="flex flex-wrap">
+                      <p className="font-semibold pr-2">Автор</p>
+                      <p>{data[0].author}</p>
+                    </span>
+                    <span className="flex flex-wrap">
+                      <p className="font-semibold pr-2">Дата</p>
+                      <time dateTime={data[0].articles[0].date}>
+                        {data[0].articles[0].date}
+                      </time>
+                    </span>
+                   
+                  </div>
+                  <span className="px-3 py-2 border border-black rounded-full w-fit">
+                    <p className="uppercase">{data[0].articles[0].label}</p>
+                  </span>
+                </div>
+              </article>
+            </article>
+            <div>
+              <Image
+                className="w-full object-cover aspect-[9/6]"
+                src={latestArticle.content[0].img}
+                alt={latestArticle.imgAlt}
+                width={1488}
+                height={992}
+                priority
               />
-            </Link>
-            <h2 className="heading3-title mb-3">
-              <Link href={`/magazine/${articleData.slug}`}>
-                {articleData.title}
-              </Link>
-            </h2>
-            <p className="mt-3 mb-12">{articleData.description}</p>
-            <div className="flex flex-wrap gap-4">
-              <span className="flex">
-                <p className="font-semibold pr-2">Автор</p>
-                <p>{articleData.author}</p>
-              </span>
-             
             </div>
           </article>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+
+        <div className="flex flex-col lg:flex-row gap-6 lg:gap-12 xl:gap-24">
+          <div className="lg:w-3/4">
+            {remainingArticles.map((article, index) => (
+              <article key={article.title}>
+                <article className="grid md:grid-cols-[0fr_1fr] gap-6 sm:gap-12">
+                  <Link href={`magazine/${article.slug}`} className="h-60 w-60">
+                    <Image
+                      className="w-full h-full object-cover hover:scale-105 transition"
+                      src={article.img}
+                      alt={article.imgAlt}
+                      width={240}
+                      height={240}
+                    />
+                  </Link>
+                  <article className="flex flex-col justify-between">
+                    <div className="mb-4 :md:mb-0">
+                      <h3 className="heading3-title mb-3">
+                        <Link href={`/magazine/${article.slug}`}>
+                          {article.title}
+                        </Link>
+                      </h3>
+
+                      <p>{article.description}</p>
+                    </div>
+                    <div className="flex flex-col sm:flex-row gap-4 justify-between sm:items-center">
+                      <div className="flex flex-col sm:flex-row gap-2 sm:gap-6">
+                        <span className="flex flex-wrap">
+                          <p className="font-semibold pr-2">Автор</p>
+                          <p>{data[0].author}</p>
+                        </span>
+                        <span className="flex flex-wrap">
+                          <p className="font-semibold pr-2">Дата</p>
+                          <time dateTime={article.date}>{article.date}</time>
+                        </span>
+                       
+                      </div>
+                      <span className="px-3 py-2 border border-black rounded-full w-fit">
+                        <p className="uppercase">{article.label}</p>
+                      </span>
+                    </div>
+                  </article>
+                </article>
+                {index < remainingArticles.length - 1 && (
+                  <Separator className="border border-black my-6" />
+                )}
+              </article>
+            ))}
+          </div>
+          <div className="lg:w-1/4">
+            <Sidebar />
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return <Loading />;
+  }
 }
