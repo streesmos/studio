1f414ac96d2ac0f5097320d49b5b997639348cd4
streesmos <61740996+streesmos@users.noreply.github.com>
2025-07-23 12:40:54 +0300

    Add meta tags and optimize image

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type PageTitleProps = {
   children: React.ReactNode;
   className?: string;
@@ -15,9 +17,11 @@ export default function PageTitle({
     <div className="max-w-[95rem] w-full mx-auto">
       <h1 className={className}>{children}</h1>
       {imgSrc && (
-        <img
+        <Image
           src={imgSrc}
           alt={imgAlt}
+          width={1488}
+          height={211}
           className="py-6 md:py-12 h-full w-full"
         />
       )}
