9ea41613f85853e4c8373f6b656b96cbed0545d1
streesmos <61740996+streesmos@users.noreply.github.com>
2025-07-23 17:47:38 +0300

    Update NewsletterSignUp.tsx

--- components/NewsletterSignUp.tsx
+++ components/NewsletterSignUp.tsx
@@ -64,7 +64,7 @@ export default function NewsletterSignUp({
           type="submit"
           className={`disabled:cursor-none ${buttonClassName}`}
         >
-          Подписаться
+          Оставить!
         </Button>
       </div>
       {state.succeeded && (
